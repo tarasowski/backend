d696fba372aecc5c375055705bc89f587ca8d4f7
mittyo <dimitri+github@tarasowski.de>
2018-04-12 07:45:34 +0200

    Cors added

--- examples/graphql-react-example/server/app.js
+++ examples/graphql-react-example/server/app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const graphHTTP = require('express-graphql') // allows express to understand graphql and run a server
 const schema = require('./schema/schema')
+const cors = require('cors')
 
 const PORT = 4000
+
+app.use(cors())
 
 app.use('/graphql', graphHTTP({
     schema,
